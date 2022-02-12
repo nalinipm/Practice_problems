a99938decc4a313a3d346a2686105b64ff9d24b0
Nalini Mehendale <nalinimehendale@Nalinis-MacBook-Pro.local>
2022-02-12 14:26:51 -0800

    Number palindrome check program

---
+++ palindromeNumber.java
@@ -0,0 +1,19 @@
+public class palindromeNumber {
+
+	public static void main(String[] args)
+	{
+		int r, sum =0, temp;
+		int n = 454;
+		
+		temp = n;
+		while(n>0) {
+			r = n%10;
+			sum = (sum*10)+r;
+			n=n/10;
+		}
+		if(temp == sum)
+			System.out.println("Palindrome number");
+		else
+			System.out.println("Not a palindrome number");
+	}
+}
