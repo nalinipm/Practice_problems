5316182e38c1d3d41ad9a18f8114c23e60c1219b
Nalini Mehendale <nalini.mehendale@sjsu.edu>
2022-07-30 07:46:42 -0700

    Added comments

--- anagram.java
+++ anagram.java
@@ -3,13 +3,18 @@ public class anagram {
 	
 	   
 	  
-	    static void isAnagram(String str1, String str2) {  
+	    static void isAnagram(String str1, String str2) { 
+	    	//Removes all blank spaces
 	        String s1 = str1.replaceAll("\\s", "");  
 	        String s2 = str2.replaceAll("\\s", "");  
+	        
+	        //Length check
 	        boolean status = true;  
 	        if (s1.length() != s2.length()) {  
 	            status = false;  
 	        } else {  
+	        	/**if length is same then convert to character array, sort and 
+	        	check if they are equal**/
 	            char[] ArrayS1 = s1.toLowerCase().toCharArray();  
 	            char[] ArrayS2 = s2.toLowerCase().toCharArray();  
 	            Arrays.sort(ArrayS1);  
